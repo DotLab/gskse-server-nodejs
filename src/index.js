c302182558c5cf1aad60043fbcf59b723b4e3dc3
Kailang <kailangfu@gmail.com>
2019-02-01 03:44:33 -0600

    Add mock data

--- src/index.js
+++ src/index.js
@@ -23,7 +23,111 @@ const Article = mongoose.model('Article', {
   sourceUrl: String,
   markdown: String,
   date: Date,
+  // cache
+  creatorName: String,
+  voteCount: Number,
+  upVoteCount: Number,
+  downVoteCount: Number,
+  loveCount: Number,
+  commentCount: Number,
+  viewCount: Number,
 });
+
+const Comment = mongoose.model('Comment', {
+  authorId: ObjectId,
+  targetId: ObjectId,
+  text: String,
+  date: Date,
+  // cache
+  authorName: String,
+  voteCount: Number,
+  replyCount: Number,
+});
+
+const Flag = mongoose.model('Flag', {
+  intent: {type: String, enum: ['UpVote', 'DownVote', 'Love', 'View']},
+  sourceId: ObjectId,
+  targetId: ObjectId,
+  date: Date,
+});
+
+if (process.env.NODE_ENV === 'development') {
+  const {articleMarkdown2} = require('./mocks');
+
+  let user;
+  let article;
+  let comment;
+
+  User.remove({}).then(() => {
+    return User.create({
+      name: 'Kailang',
+      email: 'kailangfu@gmail.com',
+      salt: '5RyOpUlzGzKL6Bhv/gPpiOtiGmQX5qxF4PqbZvO60HaxwmAjLBcGV3RHUBV8nJ83UxiYjISFdqOxHPM8D1eGsNWs/mee7GQkppBz8j2o3cVseDF5j8cFcToglY70n7FrlmPhKP50T/1H2UZAczl/g4nXTcNeE/EUbAhL6gaY4Nq01LWS6B9k60z5VhYmnup4kAX4wwUG2k30TBwvvxRi4iTk9D66VK7tEyEv/JevAwvYUmMdVP1UKpmOCS7IRLZrv9cKnGbZF/F6i19uEWZpAkOriBtOu57CfP7RKUsjzTsZqE15H/63gREe2v2nldrOstqz6FEy2yzOeWi8vZvUAA==',
+      hash: 'nlEDz8iDHQzTk75KpoQMF0+g1/KI89JDpR0Q4ciDG+oi2bMuhDOv3B48J7X1x085fEXFkvIS3b/PD+mRaow8zw==',
+    });
+  }).then((doc) => {
+    user = doc;
+    return Article.remove({}).then(() => {
+      return Article.create({
+        creatorId: user._id,
+        title: 'Rimuru Tempest and Robinson Crusoe: How to Build a Civilization',
+        excerpt: 'I remember reading The Swiss Family Robinson over and over when I was little. Washed ashore in a strange land, marooned away from everything comfortable and familiar, a family must learn to thrive in their new unfamiliar home. Unlike the post-disaster tales saturating our contemporary mediascape, where people must do drastic things to survive savage environs, The Swiss Family Robinson is a story where the land is not altogether hostile, and the characters are optimistic about their future, acting more cooperative than territorial and more curious than fearful.',
+        coverUrl: 'https://cdn.animenewsnetwork.com/thumbnails/fit600x1000/cms/feature/142729/slime-reincarnation-fantasy-13.jpg',
+        isOriginal: false,
+        sourceName: 'Anime News Network',
+        sourceUrl: 'https://www.animenewsnetwork.com/feature/2019-01-30/rimuru-tempest-and-robinson-crusoe-how-to-build-a-civilization/.142729',
+        markdown: articleMarkdown2,
+        date: new Date(),
+        // cache
+        creatorName: doc.name,
+        voteCount: 0,
+        upVoteCount: 0,
+        downVoteCount: 0,
+        loveCount: 0,
+        commentCount: 1,
+        viewCount: 0,
+      });
+    });
+  }).then((doc) => {
+    article = doc;
+    return Comment.remove({}).then(() => {
+      return Comment.create({
+        authorId: user._id,
+        targetId: article._id,
+        text: 'lask dflkas dflkas dfklasjdflkas jflkas dfalskd jfaflksd d',
+        date: new Date(),
+        // cache
+        authorName: user.name,
+        voteCount: 0,
+        replyCount: 1,
+      });
+    });
+  }).then((doc) => {
+    comment = doc;
+    return Comment.create([
+      {
+        authorId: user._id,
+        targetId: comment._id,
+        text: 'lask as dfalskd jfafasdfas sad lksd d',
+        date: new Date(),
+        // cache
+        authorName: user.name,
+        voteCount: 0,
+        replyCount: 0,
+      },
+      {
+        authorId: user._id,
+        targetId: comment._id,
+        text: 'lask as dfalskd jfaflkas dads as dd fsd d',
+        date: new Date(),
+        // cache
+        authorName: user.name,
+        voteCount: 0,
+        replyCount: 0,
+      },
+    ]);
+  });
+}
 
 const io = require('socket.io');
 const server = io.listen(6001);
@@ -32,7 +136,7 @@ debug('server start');
 const crypto = require('crypto');
 
 function success(data) {
-  debug('    success', data);
+  debug('    success');
   return {status: 'success', data};
 }
 
@@ -117,6 +221,26 @@ server.on('connect', function(socket) {
       }));
       done(success(data));
     }).catch((err) => done(error(err)));
+  });
+
+  socket.on('cl_get_article', ({title}, done) => {
+    debug('cl_get_article', title);
+    Article.findOne({title}).then((doc) => {
+      if (!doc) throw new Err('no article found');
+      const {title, excerpt, date, coverUrl, markdown, creatorName, viewCount, upVoteCount, downVoteCount, loveCount, commentCount} = doc;
+      done(success({id: doc.id, title, excerpt, date, coverUrl, markdown, creatorName, viewCount, upVoteCount, downVoteCount, loveCount, commentCount}));
+    }).catch((err) => done(error(err)));
+  });
+
+  socket.on('cl_get_comments', ({targetId}, done) => {
+    debug('cl_get_comments', targetId);
+    Comment.find({targetId}).then((docs) => {
+      if (!docs) return done(success([]));
+      return done(success(docs.map((doc) => {
+        const {text, date, authorName, voteCount, replyCount} = doc;
+        return {id: doc.id, text, date, authorName, voteCount, replyCount};
+      })));
+    });
   });
 
   socket.on('disconnect', () => {
---
+++ src/mocks.js
@@ -0,0 +1,48 @@
+exports.articleMarkdown1 = `
+Today, Apple opens its doors to 11 female-founded app development companies for the inaugural session of Entrepreneur Camp. The program is designed to provide female app creators with the tools needed to thrive in today’s global app economy. Since the App Store launched in 2008, developers have earned $120 billion, with more than a quarter of that in the past year alone.
+
+![](https://www.apple.com/newsroom/images/values/diversity-inclusion/Apple-Entrepreneur-camp-Kim-Azzarelli-01282019_carousel.jpg.large.jpg)
+
+An industry first, Entrepreneur Camp is a two-week immersive hands-on technology lab where participants work one-on-one with Apple experts and engineers to significantly accelerate their apps. The program also includes sessions on design, technology and App Store marketing, as well as ongoing guidance and support from an Apple Developer Relations representative. The curriculum will help developers take advantage of cutting-edge Apple technologies, including optimizing for the A12 Bionic chip, integrating Core ML, building new experiences with ARKit and more. Each participating company will also receive two tickets to WWDC and complementary membership to the Apple Developer Program for one year.
+
+> “The App Store is the new digital Main Street, and creative developers are tapping into the vast potential of the global app economy.”
+
+“In the past, starting a small business often meant having to invest in overhead, inventory or retail space. Today, a world of opportunity opens up with some coding skills and an entrepreneurial spirit,” said Esther Hare, Apple’s senior director, Worldwide Developer Marketing and executive sponsor, Women@Apple. “The App Store is the new digital Main Street, and creative developers are tapping into the vast potential of the global app economy. We hope that this program helps to inspire women around the world to learn to code, join the iOS development community and share in the thriving app economy.”
+
+![](https://www.apple.com/newsroom/images/values/diversity-inclusion/Apple-Entrepreneur-camp-Samantha-John-01282019_carousel.jpg.large.jpg)
+
+The apps selected for the first session of Entrepreneur Camp are: Bites by Warehouse Apps LLC; Camille by Ohhh, Inc.; CUCO: Lembrete de Medicamentos by CUCO Health; Deepr by Mental Mobile, LLC; D’efekt by Tatevik Gasparyan, Nané Toumanian and Vahagn Khachikyan; Hopscotch by Hopscotch Technologies; LactApp by LactApp Women Health; Pureple by Iceclip LLC; Statues of the La Paz Malecón by Estudio Chispa; WeParent by FamTerra Inc.; and Seneca Connect by Seneca Women.
+
+Entrepreneur Camp will hold sessions on a quarterly basis, with up to 20 app companies accepted for each round. To be admitted to the program, app-driven businesses must be female founded, co-founded or led, and have at least one woman on the product development team. They must also have a working app or prototype. For more information on Apple Entrepreneur Camp or to apply, visit [developer.apple.com/entrepreneur-camp](https://developer.apple.com/entrepreneur-camp).
+
+### Media
+Images of Entrepreneur Camp:
+[Download All](https://www.apple.com/newsroom/images/values/diversity-inclusion/Images_of_Entrepreneur-Camp.zip)
+`;
+
+exports.articleMarkdown2 = `
+I remember reading The Swiss Family Robinson over and over when I was little. Washed ashore in a strange land, marooned away from everything comfortable and familiar, a family must learn to thrive in their new unfamiliar home. Unlike the post-disaster tales saturating our contemporary mediascape, where people must do drastic things to survive savage environs, The Swiss Family Robinson is a story where the land is not altogether hostile, and the characters are optimistic about their future, acting more cooperative than territorial and more curious than fearful. The book begins in fair weather with enough food to get by, so our protagonists begin exploring. In this world, they are allowed to marvel, and in That Time I Got Reincarnated as a Slime, readers are invited to do the same.
+
+![](https://cdn.animenewsnetwork.com/thumbnails/fit600x1000/cms/feature/142729/slime-reincarnation-fantasy-13.jpg)
+
+So The Swiss Family Robinson isn't too different from That Time I Got Reincarnated as a Slime (henceforth Slime Isekai because the official title is a mouthful) at heart. Deadly disaster strikes a modern-day Japanese salaryman, reincarnating him into a strange land as a powerful slime. Finding himself relatively well off in his new body, the slime monster later named Rimuru Tempest decides to sate his curiosity and explore. He finds other monsters with sentience like himself, living in fear and tatters. Like his last name suggests, the changes he brings about for them are as paradigm-shifting as the eponymous Shakespearean storm. He builds a nation of monsters by introducing his ideas about civilization in this anime Robinsonade.
+
+![](https://cdn.animenewsnetwork.com/thumbnails/fit600x1000/cms/feature/142729/slime-reincarnation-fantasy-12.jpg)
+
+The Swiss Family Robinson and the Slime Isekai are offshoots of the romantic "Robinsonades," a genre of fiction based around the original Robinson Crusoe story. Robinson Crusoe is the adventure of the eponymous hero, who gets marooned in a strange land and thrust into an uncertain situation. But he not only survives his initial misfortunes, he thrives in his surroundings and makes them into his home-away-from-home. The descriptions of exotic phenomena in his account are exciting to imagine, and the challenges that his discoveries pose invite the reader to cleverly solve the puzzles ahead.
+
+This describes the appeal of swords-and-sorcery fantasy worlds just as well as the more conventional Robinsonade settings. And yet, for as charming as Robinsonades can be, these narratives carry baggage that not even the Slime Isekai anime has managed to move past. So what is a "civilization" really, and how does Slime Isekai recreate our ideas of civilization, good and bad?
+
+![](https://cdn.animenewsnetwork.com/thumbnails/fit600x1000/cms/feature/142729/slime-reincarnation-fantasy-1.jpg)
+![](https://cdn.animenewsnetwork.com/thumbnails/fit600x1000/cms/feature/142729/slime-reincarnation-fantasy-2.jpg)
+
+Slime Isekai has several memorable moments, and the first at the beginning comes when our protagonist slime makes friends with a towering but lonely dragon. After the curious Rimuru makes it out his starting area, he runs into diminutive and frightened goblins. They speak like him and plead for mercy. Promising them no harm, he is guided to their small village. He agrees to help the goblins defend their home against direwolves, and so defeats their leader. When the direwolves submit, they find themselves shoulder-to-shoulder with their former goblin foes.
+
+Both monster races look to Rimuru with great expectations. Finding himself unable to leave them be, he takes both groups under his wing and tells them to live with each other from now on. Throughout this odd saga, he makes a discovery: goblins and direwolves don't have names. All monsters lack names unless given by someone who already possesses a name. Unbeknownst to Rimuru at the time, names have a mana cost in this world, and most monsters can tell each other apart without them. But in Rimuru's previous life, everyone had names by default, and nothing about these monsters suggests they don't deserve the same courtesy. So Rimuru names as many of them as he can before passing out from mana loss.
+
+![](https://cdn.animenewsnetwork.com/thumbnails/fit600x1000/cms/feature/142729/slime-reincarnation-fantasy-4.jpg)
+
+So what is in a name? Delighted to have found a friend despite his attempts at being intimidating, the dragon and the slime offer each other names. Veldora the dragon is rechristened Veldora Tempest, and the salaryman-turned-slime is rebranded Rimuru Tempest. Fitting of their last names, the encounter between these two sets the dramatic events of the anime in motion. Before the encounter concludes, the rechristened Veldora Tempest drops his hot-and-cold tsundere act and declares that he and this slime are equals. Names don't just carry the function of telling different people apart; they also function to further define individuals, in terms of the civilization idealized by the progressive philosophies of the Enlightenment Era. Names do not define everything about a person, but they make it more difficult to overgeneralize and discriminate based on other factors about them. They make it harder to claim this goblin or that direwolf are mere monsters to hate or fear. More than just being practically useful, to have a name carries an implication of dignity, respect, and worth, especially in a world where having a name is optional and typically reserved for human races.
+
+As with many Robinsonades, Slime Isekai doesn't emerge fully unscathed from the sins of its troubled lineage. Despite these troubles, it remains a fascinating show. It's full of passionately animated fights and consistently good character writing. The story's world-building is also full of engrossing charm and mysteries that I want to see unraveled. The anime has thought-provoking ideas and interesting things to say about the value of names. It also has poignant observations on the value of diversity and cooperation. It illustrates a world where people of different backgrounds come together, pool their skills, and form a strong and vibrant society in the process.
+`;
